e2d8efbee8e924bc57f060892bf60f1a055af6f6
misaelAmartinezP <mmartinez@alhel.com.mx>
2022-12-28 17:16:00 -0600

    sirve arrasta

---
+++ index.js
@@ -0,0 +1,9 @@
+const dropArea = document.querySelector(".drop-area");
+const dragText = dropArea.querySelector('h2');
+const button = dropArea.querySelector('button');
+const input = dropArea.querySelector('#input-file');
+
+
+button.addEventListener('click', e =>  {
+    console.log("click");
+});
