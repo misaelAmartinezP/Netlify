782516439f5e9faecca8e13150f382386a403dbd
misaelAmartinezP <mmartinez@alhel.com.mx>
2022-12-29 10:22:11 -0600

    validar pdf

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function showFiles(files) {
 
 function processFile(file) {
     const docType = file.type;
-    const validExtensions = (".pdf");
+    const validExtensions = /(.pdf)$/i;
 
     if (validExtensions.includes(docType)) {
         //archivo valido
