8b28fe0926177d4286c3a712d8cea6f5bf288670
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-02 17:17:04 -0600

    prueba 1

--- main.js
+++ main.js
@@ -1,3 +1,66 @@
-import BudgetTracker from "./BudgetTracker.js";
+const dropArea = document.querySelector(".drop-area");
+const dragText = dropArea.querySelector('h2');
+const button = dropArea.querySelector('button');
+const input = dropArea.querySelector('#input-file');
 
-new BudgetTracker("#app");
+
+button.addEventListener('click', (e) => {
+    input.click();
+});
+
+input.addEventListener("change", (e) => {
+    file = this.files;
+    dropArea.classList.add("active");
+    showFile(files);
+    dropArea.classList.remove("active");
+})
+
+dropArea.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    dropArea.classList.add("active");
+    dragText.textContent = "Suelta para subir los archivos";
+});
+
+dropArea.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    dropArea.classList.remove("active");
+    dragText.textContent="Arrastra y suelta archivo"
+});
+
+dropArea.addEventListener("drop", (e) => {
+    e.preventDefault();
+    files = e.dataTransfer.files;
+    showFiles(files)
+    dropArea.classList.remove("active");
+    dragText.textContent = "Arrastra y suelta archivo"
+});
+
+function showFiles(files) {
+    if (files.length == undefined) {
+        processFile(files);
+    }
+    else {
+        for (const file of files) {
+            processFile(file)
+        }
+    }
+}
+
+function processFile(file) {
+    const docType = file.type;
+    const validExtensions = ('application/pdf');
+
+    if (validExtensions.includes(docType)) {
+        //archivo valido
+    } else {
+        alert('El archivo tiene que ser un pdf');
+    }
+}
+
+function readImage(file) {
+    const reader = new FileReader();
+    reader.addEventListener('load', (event) => {
+        img.src = event.target.result;
+    });
+    reader.readAsDataURL(file);
+}
