5db5704031ee7498cdcc71d50dcb81e4981e00b5
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-02 17:36:01 -0600

    prueba con boton

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const dropArea = document.querySelector(".drop-area");
 const dragText = dropArea.querySelector('h2');
+const button = dropArea.querySelector('button');
 const input = dropArea.querySelector('#input-file');
 
 
