1218c9a9eab5f6519a952aabb4095a5bc56c56ca
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-04 18:39:43 -0600

    prueba para manejo de datos

--- main.js
+++ main.js
@@ -83,7 +83,7 @@ function pdfAsArray(pdfAsArray) {
 
                 var div = document.getElementById('output');
 
-                div.innerHTML += (outputStr + pagesText[pageNum]+ "prueba 111111");
+                div.innerHTML += (outputStr + "prueba 111111");
 
             }
         });
