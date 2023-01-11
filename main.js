b076b0753eb6ebf8ca97665300dcafe0c7f15f8f
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-11 12:39:26 -0600

    prueba

--- main.js
+++ main.js
@@ -6,4 +6,16 @@ const app = express();
 
 app.use("/", express.static("index.html"));
 app.use(fileUpload());
+
+app.post("/extract-text", (req, res) => {
+    if (!req.files && !req.files.pdfFile) {
+        res.status(400);
+        res.end();
+    }
+
+    pdfParse(req.files.pdfFile).then(result => {
+        res.send(result.text);
+    })
+})
+
 app.listen(3000);
