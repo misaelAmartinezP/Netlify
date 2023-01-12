494ef72029390d170a57b3fc1ccb06cde5420984
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-11 18:42:17 -0600

    prueba razon social

--- main.js
+++ main.js
@@ -121,7 +121,7 @@ function extraeRaSo(string) {
     console.log(arrInfo.indexOf("Social:"));
     if (arrInfo.indexOf("Social:") == true) {
         for (i = arrInfo.indexOf("Social:"); i < arrInfo.indexOf("Régimen"); i++) {
-            guardaRaSo.push(i)
+            guardaRaSo.push(i+1)
         }
     }
     
