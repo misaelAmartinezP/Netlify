be2f01f6f3d0cc442b3e5bdd151adad2c1a34734
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-12 12:37:56 -0600

    codigo postal

--- main.js
+++ main.js
@@ -15,37 +15,27 @@ function ExtractText() {
 var BASE64_MARKER = ';base64,';
 
 function convertDataURIToBinary(dataURI) {
-
     var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
     var base64 = dataURI.substring(base64Index);
     var raw = window.atob(base64);
     var rawLength = raw.length;
     var array = new Uint8Array(new ArrayBuffer(rawLength));
-
     for (var i = 0; i < rawLength; i++) {
         array[i] = raw.charCodeAt(i);
     }
     pdfAsArray(array)
-
 }
 
 function getPageText(pageNum, PDFDocumentInstance) {
-    // Return a Promise that is solved once the text of the page is retrieven
     return new Promise(function (resolve, reject) {
         PDFDocumentInstance.getPage(pageNum).then(function (pdfPage) {
-            // The main trick to obtain the text of the PDF page, use the getTextContent method
             pdfPage.getTextContent().then(function (textContent) {
                 var textItems = textContent.items;
                 var finalString = "";
-
-                    // Concatenate the string of the item to the final string
                     for (var i = 0; i < textItems.length; i++) {
                         var item = textItems[i];
-
                         finalString += item.str + " "; //separador
                     }
-
-                    // Solve promise with the text retrieven from the page
                     resolve(finalString);
                 });
         });
@@ -53,45 +43,27 @@ function getPageText(pageNum, PDFDocumentInstance) {
 }
 
 function pdfAsArray(pdfAsArray) {
-
     PDFJS.getDocument(pdfAsArray).then(function (pdf) {
-
         var pdfDocument = pdf;
-        // Create an array that will contain our promises
         var pagesPromises = [];
-
         for (var i = 0; i < pdf.pdfInfo.numPages; i++) {
-            // Required to prevent that i is always the total of pages
             (function (pageNumber) {
-                // Store the promise of getPageText that returns the text of a page
                 pagesPromises.push(getPageText(pageNumber, pdfDocument));
             })(i + 1);
         }
-
-        // Execute all the promises
         Promise.all(pagesPromises).then(function (pagesText) {
-
-            // Display text of all the pages in the console
-            // e.g ["Text content page 1", "Text content page 2", "Text content page 3" ... ]
-            //console.log(pagesText); // representing every single page of PDF Document by array indexing
             let arr = (pagesText);
             srtPdf = (arr.toString());
-            //console.log(srtPdf);
-            parceoDatos(srtPdf);
-            //console.log(pagesText.length);
+            parceoDatos(srtPdf);//imprime las funciones
             var outputStr = "";
             for (var pageNum = 0; pageNum < pagesText.length; pageNum++) {
-                //console.log(pagesText[pageNum]);
                 outputStr = "";
                 outputStr = "<br/><br/>Page " + (pageNum + 1) + " contents <br/> <br/>";
                 var div = document.getElementById('output');
-
                 //div.innerHTML += (outputStr + pagesText[pageNum]);//esta linea de codigo se encarga de imprimir el contenido del archivo 
             }
         });
-
     }, function (reason) {
-        // PDF loading error
         console.error(reason);
     });
 }
@@ -99,8 +71,9 @@ function pdfAsArray(pdfAsArray) {
 function parceoDatos(string) {
     separaDatos = string.split(" ");
     console.log(separaDatos);
-    extraeRFC(separaDatos)
-    extraeRaSo(separaDatos)
+    extraeRFC(separaDatos)//extrae RFC
+    extraeRaSo(separaDatos)//extrae razon social
+    extraeCodPos(separaDatos)//extrae codigo postal
 }
 
 function extraeRFC(string) {
@@ -134,7 +107,7 @@ function extraeRaSo(string) {
     div.innerHTML += ("<br/>" + guardaRaSo.join(' ')+"<br/>");
 }
 
-function codPos(string) {
+function extraeCodPos(string) {
     arrInfo = Object.values(string)
     console.log("estoy en la funcion extrae razon social ")
     var guardacodPos = [];
