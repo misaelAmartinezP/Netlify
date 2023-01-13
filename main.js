6a1dd0ecc49b4ff91ef67b05b8bab767120fae81
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-13 10:08:16 -0600

    comentario del codigo de js

--- main.js
+++ main.js
@@ -85,148 +85,148 @@ function parceoDatos(string) {
 }
 
 function extraeRFC(string) { //funcion que extrae el pdf 
-    arrInfo = Object.values(string)
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
     for (i = 0; i < arrInfo.length; i++) { //recorrido del arreglo
         if (arrInfo[i]=="RFC:") { //cuando el indice i del arreglo seo igual a rfc
-            console.log(arrInfo[i + 1]);  //imprime el contenido del indice+1 para obtener el rfc
+            //console.log(arrInfo[i + 1]);  //imprime el contenido del indice+1 para obtener el rfc en consola
             var div = document.getElementById('output'); //variable para despues mandar el contenido que obtuvimos
             div.innerHTML += ("<br/>" + arrInfo[i + 1] + "<br/>"); //imprime el contenido del arreglo el que se encuentra en la posicion i+1
-            break
+            break //salimos del bucle
         }
     }
 }
 
-function extraeRaSo(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae razon social ")
-    var guardaRaSo = [];
-    console.log(arrInfo.indexOf("Contribuyentes"));
-    console.log(arrInfo.indexOf("Nombre,"));
-    if (arrInfo.includes("Contribuyentes") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Contribuyentes")+1; i <= arrInfo.indexOf("Nombre,")-1; i++) {
-            console.log(i);
-            guardaRaSo.push(arrInfo[i]);
+function extraeRaSo(string) { //funcion que extrae la razon social 
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae razon social ") //imprime el nombre de la funcion en consola 
+    var guardaRaSo = []; //declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Contribuyentes"));//busqueda el indice de la palabra
+    //console.log(arrInfo.indexOf("Nombre,"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Contribuyentes") == true) { //si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion 
+        //console.log("estoy en el if y soy true") //verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Contribuyentes") + 1; i <= arrInfo.indexOf("Nombre,") - 1; i++) {//recorrido de indices para obtener la informacion necesaria
+            //console.log(arrInfo[i]); //ver en consola si el contenido es el esperado 
+            guardaRaSo.push(arrInfo[i]); //guarda en el arreglo guardaRaSo el contenido del indice del arreglo arrInfo en la posicion i 
         } 
     } else {
         var div = document.getElementById('output');
-        div.innerHTML += ("<br/> 'EL PDF NO ES UNA CONSTANCIA FISCAL'");
+        div.innerHTML += ("<br/> 'EL PDF NO ES UNA CONSTANCIA FISCAL'");//valida que el pdf sea una constancia fiscal y lo imprime en la pagina web 
     }    
     var div = document.getElementById('output');
-    div.innerHTML += ("<br/>" + guardaRaSo.join(' ')+"<br/>");
+    div.innerHTML += ("<br/>" + guardaRaSo.join(' ') + "<br/>");//imprime el contenido en la pagina web
 }
 
-function extraeCodPos(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae codigo postal ")
-    var guardaCodPos = [];
-    console.log(arrInfo.indexOf("Postal:"));
-    console.log(arrInfo.indexOf("Tipo"));
-    if (arrInfo.includes("Postal:") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Postal:") + 1; i <= arrInfo.indexOf("Tipo") - 1; i++) {
-            console.log(i);
-            guardaCodPos.push(arrInfo[i]);
+function extraeCodPos(string) { //funcion que extrae el codigo postal 
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo  
+    //console.log("estoy en la funcion extrae codigo postal ")//imprime el nombre de la funcion en consola 
+    var guardaCodPos = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Postal:"));//busqueda el indice de la palabra
+    //console.log(arrInfo.indexOf("Tipo"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Postal:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Postal:") + 1; i <= arrInfo.indexOf("Tipo") - 1; i++) {//recorrido de indices para obtener la informacion necesaria
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+            guardaCodPos.push(arrInfo[i]);//guarda en el arreglo guardaCodPos el contenido del indice del arreglo arrInfo en la posicion i
         }
     }
     var div = document.getElementById('output');
-    div.innerHTML += ("<br/>" + guardaCodPos.join(' ') + "<br/>");
+    div.innerHTML += ("<br/>" + guardaCodPos.join(' ') + "<br/>"); //imprime el contenido en la pagina web
 }
 
-function extraeNomVia(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae nombre vialidad")
-    var guardaNomVia = [];
-    console.log(arrInfo.indexOf("Exterior:"));
-    if (arrInfo.includes("Exterior:") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Exterior:")-2; i >= arrInfo.indexOf("Vialidad:"); i--) {
-            if (arrInfo[i] == "Vialidad:") {
+function extraeNomVia(string) { //funcion que extrae nombre de vialidad
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae nombre vialidad")//imprime el nombre de la funcion en consola 
+    var guardaNomVia = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    console.log(arrInfo.indexOf("Exterior:"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Exterior:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Exterior:")-2; i >= arrInfo.indexOf("Vialidad:"); i--) { //recorrido para encontrar la informacion que necesitamos 
+            if (arrInfo[i] == "Vialidad:") {//si encontramos que el idice de la posicion arrInfo[i] es igual a Vialidad: termina el recorrido 
                 break
             }
-            guardaNomVia.push(arrInfo[i]);
-            console.log(arrInfo[i]);
+            guardaNomVia.push(arrInfo[i]);//guarda en el arreglo guardaNomVia el contenido del indice del arreglo arrInfo en la posicion i
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
         }
     }
-    console.log(guardaNomVia.reverse());
+    console.log(guardaNomVia.reverse());//invierte el contenido del arreglo guardaNomVia
     var div = document.getElementById('output');
-    div.innerHTML += ("<br/>" + guardaNomVia.join(' ') + "<br/>");
+    div.innerHTML += ("<br/>" + guardaNomVia.join(' ') + "<br/>");//imprime el contenido en la pagina web
 }
 
-function extraeNumExt(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae numero exterior")
-    var guardaNunExt = [];
-    console.log(arrInfo.indexOf("Exterior:"));
-    if (arrInfo.includes("Exterior:") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Exterior:")+1; i <= arrInfo.length; i++) {
-            console.log(arrInfo[i]);
-            guardaNunExt.push(arrInfo[i]);
+function extraeNumExt(string) { //funcion que extrae el numero exterior
+    arrInfo = Object.values(string)  //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae numero exterior")//imprime el nombre de la funcion en consola 
+    var guardaNunExt = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Exterior:"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Exterior:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Exterior:") + 1; i <= arrInfo.length; i++) {//recorrido de indices para obtener la informacion necesaria
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+            guardaNunExt.push(arrInfo[i]);//guarda en el arreglo guardaNumExt el contenido del indice del arreglo arrInfo en la posicion i
             break
         }
     }
     var div = document.getElementById('output');
-    div.innerHTML += ("<br/>" + guardaNunExt.join(' ') + "<br/>");
+    div.innerHTML += ("<br/>" + guardaNunExt.join(' ') + "<br/>");//imprime el contenido en la pagina web
 }
 
-function extraeNumInt(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae numero interior")
-    var guardaNunInt = [];
-    console.log(arrInfo.indexOf("Interior:"));
-    if (arrInfo.includes("Interior:") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Interior:") + 1; i <= arrInfo.indexOf("Colonia:")-4; i++) {
-            console.log(arrInfo[i]);
-            guardaNunInt.push(arrInfo[i]);
+function extraeNumInt(string) {//funcion que extrae el numero interior
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae numero interior")//imprime el nombre de la funcion en consola 
+    var guardaNunInt = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Interior:"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Interior:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Interior:") + 1; i <= arrInfo.indexOf("Colonia:")-4; i++) {//recorrido de indices para obtener la informacion necesaria
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+            guardaNunInt.push(arrInfo[i]);//guarda en el arreglo guardaNumInt el contenido del indice del arreglo arrInfo en la posicion i
         }
     }
-    if (guardaNunInt.length == 0) {
-        console.log("SN")
+    if (guardaNunInt.length == 0) {//si la longitud del arreglo es igual a cero 
+        //console.log("SN") //linea de apoyo se visualiza en consola 
         var div = document.getElementById('output');
-        div.innerHTML += ("<br/> SIN NUMERO INTERIOR <br/>");
-    } else {
+        div.innerHTML += ("<br/> SIN NUMERO INTERIOR <br/>");//imprime el contenido de que la constancia fiscal no tiene numero interior en la pagina web
+    } else { //si no se cumple lo anterior entonces 
         var div = document.getElementById('output');
-        div.innerHTML += ("<br/>" + guardaNunInt.join(' ') + "<br/>");
+        div.innerHTML += ("<br/>" + guardaNunInt.join(' ') + "<br/>");//imprime el contenido en la pagina web
     }
 }
 
-function extraeNomCol(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae numero interior")
-    var guardaNomCol = [];
-    console.log(arrInfo.indexOf("Colonia:"));
-    if (arrInfo.includes("Colonia:") == true) {
-        console.log("estoy en el if y soy true")
-        for (i = arrInfo.indexOf("Colonia:") + 1; i <= arrInfo.indexOf("Localidad:") - 4; i++) {
-            console.log(arrInfo[i]);
-            guardaNomCol.push(arrInfo[i]);
+function extraeNomCol(string) {//funcion que extrae la colonia
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae numero interior")//imprime el nombre de la funcion en consola 
+    var guardaNomCol = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Colonia:"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Colonia:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+        for (i = arrInfo.indexOf("Colonia:") + 1; i <= arrInfo.indexOf("Localidad:") - 4; i++) { //recorrido de indices para obtener la informacion necesaria 
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+            guardaNomCol.push(arrInfo[i]);//guarda en el arreglo guardaNomCol el contenido del indice del arreglo arrInfo en la posicion i
         }
     }
-    if (guardaNomCol.length == 0) {
-        console.log("SN")
+    if (guardaNomCol.length == 0) {//si la longitud del arreglo es igual a cero 
+        //console.log("SN")
         var div = document.getElementById('output');
-        div.innerHTML += ("<br/> SIN NOMBRE DE COLONIA <br/>");
-    } else {
+        div.innerHTML += ("<br/> SIN NOMBRE DE COLONIA <br/>");//imprime sin nombre de colonia en la pagina web 
+    } else {//si no se cumple lo anterior entonces
         var div = document.getElementById('output');
-        div.innerHTML += ("<br/>" + guardaNomCol.join(' ') + "<br/>");
+        div.innerHTML += ("<br/>" + guardaNomCol.join(' ') + "<br/>");//imprime el contenido del arreglo guardaNomCol en la pagina web
     }
 }
 
-function extraeReg(string) {
-    arrInfo = Object.values(string)
-    console.log("estoy en la funcion extrae codigo postal ")
-    var guardaReg = [];
-    console.log(arrInfo.indexOf("Regímenes:"));
-    console.log(arrInfo.indexOf("Obligaciones:"));
-    if (arrInfo.includes("Regímenes:") == true) {
-        console.log("estoy en el if y soy true")
+function extraeReg(string) {//funcion que extrae el regimen 
+    arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+    //console.log("estoy en la funcion extrae codigo postal ")//imprime el nombre de la funcion en consola 
+    var guardaReg = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+    //console.log(arrInfo.indexOf("Regímenes:"));//busqueda el indice de la palabra
+    //console.log(arrInfo.indexOf("Obligaciones:"));//busqueda el indice de la palabra
+    if (arrInfo.includes("Regímenes:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+        //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
         for (i = arrInfo.indexOf("Regímenes:") + 8 ; i <= arrInfo.indexOf("Obligaciones:") - 2; i++) {
-            console.log(arrInfo[i]);
-            guardaReg.push(arrInfo[i]);
+            //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+            guardaReg.push(arrInfo[i]);//guarda en el arreglo guardaReg el contenido del indice del arreglo arrInfo en la posicion i
         }
     }
     var div = document.getElementById('output');
-    div.innerHTML += ("<br/>" + guardaReg.join(' ') + "<br/>");
+    div.innerHTML += ("<br/>" + guardaReg.join(' ') + "<br/>");//imprime el contenido del arreglo guardaReg en la pagina web
 }
