c2bd1992f48162c253a03146e899fe628bb5dc68
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-19 11:08:32 -0600

    prueba

--- main.js
+++ main.js
@@ -266,5 +266,5 @@ contenedor.addEventListener('drop', e => {
     e.preventDefault();
     console.log('Drop');
     dragText.textContent = "Arrastra y suelta archivo"
-    ExtractText();
+    ExtractText(file);
 });
