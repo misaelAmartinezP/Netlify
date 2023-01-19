ab6b6047556431993937ab8d1b1bd46a434e6486
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-19 12:41:42 -0600

    demarcacion territorial

--- main.js
+++ main.js
@@ -87,7 +87,7 @@ function parceoDatos(string) {
     extraeNumInt(separaDatos)//extrae numero interior
     extraeNomCol(separaDatos) //extrae nombre de la colonia
     extraeReg(separaDatos)//extrae regimen
-    extraeDemTer(string)//extrae demarcacion territorial o municipio
+    extraeDemTer(separaDatos)//extrae demarcacion territorial o municipio
 }
 
 function extraeRFC(string) { //funcion que extrae el pdf 
@@ -242,10 +242,10 @@ function extraeReg(string) {//funcion que extrae el regimen
 
 function extraeDemTer(string) {//funcion que extrae la demarcacion terriotorial o el municipio 
     arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
-    console.log("estoy en la funcion extrae demarcacion o delegacion")//imprime el nombre de la funcion en consola 
+    console.log("estoy en la funcion extrae codigo postal ")//imprime el nombre de la funcion en consola 
     var guardaReg = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
-    console.log(arrInfo.indexOf("Exterior:"));//busqueda el indice de la palabra
-    console.log(arrInfo.indexOf("Exterior:"));//busqueda el indice de la palabra
+    console.log(arrInfo.indexOf("Territorial:"));//busqueda el indice de la palabra
+    console.log(arrInfo.indexOf("Federativa:"));//busqueda el indice de la palabra
     if (arrInfo.includes("Territorial:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
         //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
         for (i = arrInfo.indexOf("Territorial:") + 1; i <= arrInfo.indexOf("Federativa:") - 4; i++) {
