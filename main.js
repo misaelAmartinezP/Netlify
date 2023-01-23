939e390432becf4b2bddb76d65a55d5e428cee06
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-23 15:36:57 -0600

    prueba 3/4 caracteres de cada palabra

--- main.js
+++ main.js
@@ -161,24 +161,21 @@ function creaGenCod(string, string1) {
         if ((arrAux.length) >= 4) {
             arrAux[0].substring(0, 3);
             console.log(arrAux[0].substring(0, 3));
-            //arrAux[1].substring(0, 3);
+            arrAux[1].substring(0, 3);
             console.log(arrAux[1].substring(0, 3));
-            //arrAux[2].substring(0, 3);
+            arrAux[2].substring(0, 3);
             console.log(arrAux[2].substring(0, 4));
-            //arrAux[3].substring(0, 4);
-            console.log(arrAux[3].substring(0, 3));
+            arrAux[3].substring(0, 4);
         }
     }
     else if ((difPer) == 13) {//si es persona fisica 
         if ((arrAux.length) == 4) {
-            arrAux[0].substring(0, 3);
-            console.log(arrAux[0].substring(0, 3));
-            //arrAux[1].substring(0, 3);
-            console.log(arrAux[1]);
-            //arrAux[2].substring(0, 3);
-            console.log(arrAux[2]);
-            //arrAux[3].substring(0, 4);
-            console.log(arrAux[3]);
+            console.log(arrAux[2].substring(0, 3));
+            arrAux[3].substring(0, 3);
+            console.log(arrAux[4].substring(0, 3));
+            arrAux[3].substring(0, 3);
+            console.log(arrAux[0].substring(0, 4));
+            arrAux[3].substring(0, 4);
         }
         console.log(arrCodigo);//codigo listo 
     }
