9496d14fa9bbbbc19264ff94477f2d6a36341724
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-23 11:39:32 -0600

    recorrido

--- main.js
+++ main.js
@@ -150,7 +150,7 @@ function creaGenCod(string, string1) {
     for (i in arrAux) {
         console.log(arrAux[i]);
         for (j in arrAux[i]) {
-            console.log(j);
+            console.log(arrAux[j]);
         }
     }
 }
