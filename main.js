a37bbaf6419fe206a4a4a97a6c11fdab719f4827
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-01-23 09:53:26 -0600

    generacion codigo

--- main.js
+++ main.js
@@ -150,13 +150,12 @@ function creaGenCod(string, string1) {
 
 
     for (j = 0; j <= arrAux.length; j++) {
-        console.log(arrAux[i]);
         for (k = 0; k <= j; k++) {
             console.log(k);
         }
     }
 
-    console.log(arrAux);//ver la separacion de la razon social
+    //console.log(arrAux);//ver la separacion de la razon social
 }
 
 
