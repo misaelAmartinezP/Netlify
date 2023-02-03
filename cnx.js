faa1d8107f94e5e3a765c169eeeed5ec2302806d
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-02-02 18:57:20 -0600

    conexion

--- cnx.js
+++ cnx.js
@@ -1,8 +1,8 @@
 const cnx = {
     user: 'sa',
     password: 'misaeL197520*',
-    server: 'localhost',
+    server: 'localhost', 
     database: '2018',
     port: 49718
 }
-module.exports = cnx;
+module.exports = cnx;  
