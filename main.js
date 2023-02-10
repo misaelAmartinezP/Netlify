3f7948f1aae02890c5ae6616c1d088e7aefa542b
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-02-10 15:51:16 -0600

    VERIFICACION DE LA CLABE

--- main.js
+++ main.js
@@ -500,8 +500,11 @@ function guardar() {
     } if (ctaBancaria == "") {//verifica que el campo cuenta bancaria no este vacio
         alert("SE REQUIERE CUENTA BANCARIA");
         document.getElementById("ctaBancaria").focus();
-    } if ((clabe.length) != 18) {
-        alert("LA LONGITUD DE LA CLABE TIENE QUE SER DE 18 DIGITOS");
+    } if ((clabe.length) > 18) {
+        alert("LA LONGITUD DE LA CLABE TIENE QUE SER IGUAL A 18 DIGITOS");
+        document.getElementById("clabe").focus();
+    } if ((clabe.length) < 18) {
+        alert("LA LONGITUD DE LA CLABE NO PUEDE TENER MAS DE 18 DIGITOS");
         document.getElementById("clabe").focus();
     } else {
         document.getElementById("banco1").value = "";
