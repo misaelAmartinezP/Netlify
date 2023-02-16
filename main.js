a364ccfe142ae2baee2dbe8f796a319d5e7a84d4
misaelAmartinezP <mmartinez@alhel.com.mx>
2023-02-16 10:35:01 -0600

    regimen capital prueba para SA de CV

--- main.js
+++ main.js
@@ -913,6 +913,31 @@ function guardarCtc() {//guarda los datos introducidos en los inputs y verifica 
     }
 }
 
+function regimenCapital(string, rfc) {
+    var regCapital = "";
+    if ((rfc.length) == 12) {
+        arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
+        //console.log("estoy en la funcion  regimen Capital")//imprime el nombre de la funcion en consola 
+        var guardaRegCap = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
+        //console.log(arrInfo.indexOf("Capital:"));//busqueda el indice de la palabra
+        //console.log(arrInfo.indexOf("Comercial:"));//busqueda el indice de la palabra
+        if (arrInfo.includes("Capital:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
+            //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
+            for (i = arrInfo.indexOf("Capital:") + 1; i <= arrInfo.indexOf("Comercial:") - 2; i++) {
+                //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
+                guardaRegCap.push(arrInfo[i]);//guarda en el arreglo guardaReg el contenido del indice del arreglo arrInfo en la posicion i
+            }
+        }
+
+        if ((guardaRegCap.join(' ')) == 'SOCIEDAD ANONIMA DE CAPITAL VARIABLE') {
+            regCapital = 'SA de CV';
+            //console.log(regCapital)
+        }
+    }
+    //console.log(regCapital);
+    return regCapital;
+}
+
 function descripcionTipOracion(gaurdaPersonaFisApPa, gaurdaPersonaFisApMa, guardaPersonaFisNom, guardaRaSo, string, guardaRegimenCapital) {//para la descripcion de cuentas es en tipo Oracion el texto
     rfc = string;
     var guardatipOracion = "";
@@ -939,31 +964,6 @@ function descripcionTipOracion(gaurdaPersonaFisApPa, gaurdaPersonaFisApMa, guard
     }
     //console.log(guardatipOracion);
     return guardatipOracion;//regresar el valor del resultado
-}
-
-function regimenCapital(string, rfc) {
-    var regCapital = "";
-    if ((rfc.length) == 12) {
-        arrInfo = Object.values(string) //convertimos el contenido de la extracion en un arreglo 
-        //console.log("estoy en la funcion  regimen Capital")//imprime el nombre de la funcion en consola 
-        var guardaRegCap = [];//declaramos un array vacio para llenarlo con la informacion necesaria del campo a buscar 
-        //console.log(arrInfo.indexOf("Capital:"));//busqueda el indice de la palabra
-        //console.log(arrInfo.indexOf("Comercial:"));//busqueda el indice de la palabra
-        if (arrInfo.includes("Capital:") == true) {//si la palabra se encuentra en el arreglo y el valor de este es verdadero entonces realiza la condicion
-            //console.log("estoy en el if y soy true")//verificar que si se cumplio la condicion linea de apoyo 
-            for (i = arrInfo.indexOf("Capital:") + 1; i <= arrInfo.indexOf("Comercial:") - 2; i++) {
-                //console.log(arrInfo[i]);//ver en consola si el contenido es el esperado
-                guardaRegCap.push(arrInfo[i]);//guarda en el arreglo guardaReg el contenido del indice del arreglo arrInfo en la posicion i
-            }
-        }
-
-        if ((guardaRegCap.join(' ')) == 'SOCIEDAD ANONIMA DE CAPITAL VARIABLE') {
-            regCapital = 'SA de CV';
-            //console.log(regCapital)
-        }
-    }
-    //console.log(regCapital);
-    return regCapital;
 }
 
 function querysCtas(string, guardaTipoOracion, guardaCtaConta1, guardaCtaConta2, guardaCtaAnt1, guardaCtaAnt2) { 
